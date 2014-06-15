0a8f9a64d946716dc4cf19cddb0a02e7415ac09e
Jed Mao <jed.hunsaker@gmail.com>
2014-06-15 10:07:12 -0500

    Implement CLI

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿var gulp = require('gulp');
+
+var clean = require('gulp-clean');
+var tsc = require('gulp-tsc');
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['clean', 'ts']);
+
+gulp.task('clean', function() {
+	return gulp.src(['lib/cli.js'], { read: false })
+		.pipe(clean());
+});
+
+gulp.task('ts', function() {
+	return gulp.src('lib/cli.ts')
+		.pipe(tsc({ target: 'es5' }))
+		.pipe(gulp.dest('lib'));
+});
